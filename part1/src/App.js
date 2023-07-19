19898cd0363d41421c440d1d1d733023a1473342
KMing02 <676906733@qq.com>
2023-07-19 21:30:33 +0800

    complete exercise 1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,45 @@
-const Hello = (props) => {
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-  console.log(props)
   return (
     <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <Header course = {course} />
+      <Content name = {part1} exercise = {exercises1} />
+      <Content name = {part2} exercise = {exercises2} />
+      <Content name = {part3} exercise = {exercises3} />
+      <Total ex1 = {exercises1} ex2 = {exercises2} ex3 = {exercises3} />
     </div>
   )
 }
 
-const App = () => {
-
-  const name = 'Peter'
-  const age = 10
-
+const Header = (props) => {
   return (
     <div>
-      <h1>Greetings</h1>
+      {props.course}
+    </div>
+  )
+}
 
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercise}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+      Number of exercises {props.ex1 + props.ex2 + props.ex3}
+      </p>
     </div>
   )
 }
