a501ed3c5277a6ac1fc3e8616deb461f14d22c25
KMing02 <676906733@qq.com>
2023-09-08 17:24:16 +0800

    blogList updates

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
   }
@@ -20,9 +22,44 @@ const favoriteBlog = (blogs) => {
 
     return blogs.reduce(helper, blogs[0])
 }
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    }
+    const authorList = _.uniq(blogs.map(b => b.author))
+    const max = {author: "", blogs: 0}
+    authorCount = _.pick(_.countBy(blogs, 'author'), authorList)
+    
+    for (let [key, value] of Object.entries(authorCount)) {
+        if (value > max.blogs) {
+            max.author = key
+            max.blogs = value
+        }
+    }
+    return max
+}
+
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    }
+    let likesCounts = blogs.reduce((likesCount, blog) => {
+        likesCount[blog.author] = (likesCount[blog.author] || 0) + blog.likes
+        return likesCount
+    }, {})
+    let maxCount = Math.max(...Object.values(likesCounts))
+    let mostLiked = Object.keys(likesCounts).filter(author => likesCounts[author] === maxCount)
+    return {
+        author: mostLiked[0],
+        likes: maxCount
+    }
+}
   
   module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
   }
