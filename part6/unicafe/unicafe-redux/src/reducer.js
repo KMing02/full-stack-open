5fd2760d29e009e56be72ef52ad134272bd7f1fa
KMing02 <676906733@qq.com>
2023-10-05 03:08:42 +0100

    part6

---
+++ part6/unicafe/unicafe-redux/src/reducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      const good = state.good + 1
+      return {...state, good: good}
+    case 'OK':
+      const ok = state.ok + 1
+      return {...state, ok: ok}
+    case 'BAD':
+      const bad = state.bad + 1
+      return {...state, bad: bad}
+    case 'ZERO':
+      return state
+    case 'RESET_STATS':
+      return initialState
+    default: return state
+  }
+  
+}
+
+export default counterReducer
